c261b9347c0c35a64d14a60df1243ae000443237
Your Name <657920579@qq.com>
2022-08-17 15:06:55 +0800

    update

--- Main.go
+++ Main.go
@@ -4,7 +4,6 @@ import (
 	"bufio"
 	"compress/gzip"
 	"flag"
-	"fmt"
 	"github.com/kxg3030/shermie-proxy/Core"
 	"github.com/kxg3030/shermie-proxy/Core/Websocket"
 	"github.com/kxg3030/shermie-proxy/Log"
@@ -38,7 +37,7 @@ func main() {
 	s := Core.NewProxyServer(*port, *nagle, *proxy)
 	// 注册http事件函数
 	s.OnHttpRequestEvent = func(request *http.Request) {
-		//fmt.Println("http请求地址：" + request.URL.Host)
+		//Log.Log.Println("http请求地址：" + request.URL.Host)
 	}
 	// 注册http事件函数
 	s.OnHttpResponseEvent = func(response *http.Response) {
@@ -50,25 +49,25 @@ func main() {
 				reader, _ = gzip.NewReader(response.Body)
 			}
 			body, _ := io.ReadAll(reader)
-			fmt.Println("http返回数据：" + string(body))
+			Log.Log.Println("http返回数据：" + string(body))
 		}
 	}
 	// 注册socket5服务器向客户端推送消息事件函数
 	s.OnSocket5ResponseEvent = func(message []byte) {
-		fmt.Println("socket5服务器发送数据", message)
+		Log.Log.Println("socket5服务器发送数据", message)
 	}
 	// 注册socket5客户端向服务器推送消息事件函数
 	s.OnSocket5RequestEvent = func(message []byte) {
-		fmt.Println("socket5客户端发送数据", message)
+		Log.Log.Println("socket5客户端发送数据", message)
 	}
 	// 注册ws服务器向客户端推送消息事件函数
 	s.OnWsRequestEvent = func(msgType int, message []byte, clientConn *Websocket.Conn, resolve Core.ResolveWs) error {
-		fmt.Println("服务器向浏览器响应数据：" + string(message) + "消息号：" + strconv.Itoa(msgType))
+		Log.Log.Println("服务器向浏览器响应数据：" + string(message) + "消息号：" + strconv.Itoa(msgType))
 		return clientConn.WriteMessage(msgType, message)
 	}
 	// 注册ws客户端向服务器推送消息事件函数
 	s.OnWsResponseEvent = func(msgType int, message []byte, tartgetConn *Websocket.Conn, resolve Core.ResolveWs) error {
-		fmt.Println("浏览器向服务器发送数据：" + string(message) + "消息号：" + strconv.Itoa(msgType))
+		Log.Log.Println("浏览器向服务器发送数据：" + string(message) + "消息号：" + strconv.Itoa(msgType))
 		return resolve(msgType, message, tartgetConn)
 	}
 	_ = s.Start()
